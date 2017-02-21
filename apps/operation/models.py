c58eaa5186adc9c77d72bd6225553ee2227e45b8
wj5633 <717728534@qq.com>
2017-02-22 00:49:24 +0800

    添加apps

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from django.db import models
 
-# Create your models here.
 from courses.models import Course
 from users.models import UserProfile
 
--- online/settings.py
+++ online/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
