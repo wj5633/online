b5996233bc4e418adab991f44bff0014f1317357
wj5633 <717728534@qq.com>
2017-03-18 16:15:56 +0800

    celery

---
+++ online/celery_ag.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*-coding=utf-8-*-
+from __future__ import unicode_literals
+
+import os
+import django
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online.settings')
+django.setup()
+
+app = Celery('online')
+app.conf.result_backend = 'redis://localhost:6379/0'
+app.conf.result_backend = 'redis://localhost:6379/0'
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
