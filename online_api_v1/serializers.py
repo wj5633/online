50723c334d05eee202971cb7c9ec304b0cb221db
wj5633 <wj199017>
2017-05-18 13:40:54 +0800

    HyperlinkedModelSerializer

--- online_api_v1/serializers.py
+++ online_api_v1/serializers.py
@@ -8,91 +8,91 @@ from operation.models import UserAsk, CourseComments, UserFavorite, UserMessage,
 from organization.models import CourseOrg, CityDict, Teacher
 
 
-class BannerSerializer(serializers.ModelSerializer):
+class BannerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Banner
         fields = '__all__'
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
 
 
-class BannerCourseSerializer(serializers.ModelSerializer):
+class BannerCourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = BannerCourse
         fields = '__all__'
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class LessonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
 
 
-class VideoSerializer(serializers.ModelSerializer):
+class VideoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Video
         fields = '__all__'
 
 
-class CourseResourceSerializer(serializers.ModelSerializer):
+class CourseResourceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CourseResource
         fields = '__all__'
 
 
-class UserAskSerializer(serializers.ModelSerializer):
+class UserAskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserAsk
         fields = '__all__'
 
 
-class CourseCommentsSerializer(serializers.ModelSerializer):
+class CourseCommentsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CourseComments
         fields = '__all__'
 
 
-class UserFavoriteSerializer(serializers.ModelSerializer):
+class UserFavoriteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserFavorite
         fields = '__all__'
 
 
-class UserMessageSerializer(serializers.ModelSerializer):
+class UserMessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserMessage
         fields = '__all__'
 
 
-class UserCourseSerializer(serializers.ModelSerializer):
+class UserCourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserCourse
         fields = '__all__'
 
 
-class TeacherSerializer(serializers.ModelSerializer):
+class TeacherSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'
 
 
-class CityDictSerializer(serializers.ModelSerializer):
+class CityDictSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CityDict
         fields = '__all__'
 
 
-class CourseOrgSerializer(serializers.ModelSerializer):
+class CourseOrgSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CourseOrg
         fields = '__all__'
